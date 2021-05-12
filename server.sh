298ccc78051dcc1580ce361e0177432afe26dbec
David Powell <david@drp.id.au>
2021-05-13 09:16:42 +1000

    Make server.sh work for python2 or python3
    - Fixes #48

--- server.sh
+++ server.sh
@@ -3,5 +3,6 @@
 echo "Run: make compile"
 echo "      (or 'make debug' for development)"
 echo "http://localhost:8030/pan.html"
-python -mSimpleHTTPServer 8030
+python -m $(python -c 'import sys; print("http.server" if sys.version_info[:2] > (2,7) else "SimpleHTTPServer")') 8030
+
 
